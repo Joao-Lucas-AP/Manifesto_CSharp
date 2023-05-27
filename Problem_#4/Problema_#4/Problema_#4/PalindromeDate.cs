c2eb169a779d334687b3e2abfea3cfcfba34336b
Joao-Lucas-AP <jlucasap@hotmail.com>
2023-05-27 15:51:34 -0300

    Problema_#4 solucionado!!
    Finished!!!

---
+++ Problem_#4/Problema_#4/Problema_#4/PalindromeDate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema__4
+{
+    public class PalindromeDate
+    {
+        public static DateTime FindLastPalindromeDate()
+        {
+            DateTime currentDate = DateTime.Now;
+
+            while (true)
+            {
+                string dateString = currentDate.ToString("MMddyyyy");
+                bool isPalindrome = IsPalindrome(dateString);
+
+                if (isPalindrome)
+                {
+                    return currentDate;
+                }
+
+                currentDate = currentDate.AddDays(-1);
+            }
+        }
+
+        public static bool IsPalindrome(string text)
+        {
+            int left = 0;
+            int right = text.Length - 1;
+
+            while(left < right)
+            {
+                if (text[left] != text[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Problem_#4/Problema_#4/Problema_#4/Program.cs
@@ -0,0 +1,12 @@
+﻿using Problema__4;
+using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    public static void Main()
+    {
+        DateTime lastPalindromeDate = PalindromeDate.FindLastPalindromeDate();
+        Console.WriteLine("The last palindrome date was: " + lastPalindromeDate.ToString("MM/dd/yyyy"));
+    }
+}
