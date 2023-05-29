4eba1fb99080e1dab483b8ae55233ceb76d516f8
Joao-Lucas-AP <jlucasap@hotmail.com>
2023-05-29 17:44:10 -0300

    Agora vai!

--- Problem_#1/Problem_#1/Problem_#1/CircleIntersection.cs
+++ Problem_#1/Problem_#1/Problem_#1/CircleIntersection.cs
@@ -1,11 +1,13 @@
-﻿public class CircleIntersection
+﻿using System.Xml.Serialization;
+
+public class CircleIntersection
 {
     public static bool CirclesIntersect(Circle circle1, Circle circle2)
     {   
-        float distance = (float)Math.Sqrt(Math.Pow(circle2.X - circle1.X, 2) + Math.Pow(circle2.Y - circle1.Y, 2));
+        float distance = (float)Math.Sqrt(Math.Pow(circle2.X - circle1.X, 2) + Math.Pow(circle2.Y - circle1.Y, 2)); //cálculo de distância euclidiana para saber a distância entre o centro dos círculos.
         
         float radiusSum = circle1.Radius + circle2.Radius;
         
-        return distance <= radiusSum;
+        return distance <= radiusSum; //Se a soma dos raios dos círculos for menor que a distância eles intersectam.
     }
 }
